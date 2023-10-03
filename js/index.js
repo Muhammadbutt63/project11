1e515c6a80b4e2252cc197edb0072fb283b3ca5e
Muhammadbutt63 <muhammadbutt637@gmail.com>
2023-10-04 00:43:22 +0500

    add

--- js/index.js
+++ js/index.js
@@ -1,10 +1,6 @@
 
-
 let mybutton = document.getElementById("myBtn");
-
-
 window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
